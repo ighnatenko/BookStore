16efbeaa2a05bbaadb670ad2e5dd0c3babb059a3
ighnatenko <ighnatenko@meta.ua>
2018-04-23 00:13:11 +0300

    add secret keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -277,5 +277,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.omniauth :facebook, ENV["fb_user_id"], ENV["fb_publishable_secret"]
+  config.omniauth :facebook, ENV["FACEBOOK_ID"], ENV["FACEBOOK_SECRET"]
 end
