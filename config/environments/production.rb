205c2cf32efbdf5f276fe6b19e1b4f529c8af264
ighnatenko <ighnatenko@meta.ua>
2018-04-27 23:07:13 +0300

    refactoring

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,13 +22,13 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'http://www.bookstore-x.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'https://bookstore-x.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'bookstore-x.heroku.com',
+    :domain => 'herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
