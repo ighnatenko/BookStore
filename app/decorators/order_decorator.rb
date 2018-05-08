2cac8419a433db74f97395e3eeeec97018ec043b
ighnatenko <ighnatenko@meta.ua>
2018-05-08 03:14:01 +0300

    add order summary

---
+++ app/decorators/order_decorator.rb
@@ -0,0 +1,20 @@
+class OrderDecorator < Draper::Decorator
+  delegate_all
+  decorates_association :book
+
+  def sale
+    coupon ? coupon.discount : 0.0
+  end
+
+  def subtotal
+    books.map(&:decorate).map(&:total_price).reduce(&:+)
+  end
+
+  def coupon_discount
+    subtotal * sale / 100
+  end
+
+  def order_total
+    subtotal - coupon_discount
+  end
+end
